ad4e30443ce15e145d57098ff62cc08c7d2b5bf9
Ruslan Ponomarenko <ruslan.p@sbtech.com>
2020-06-28 20:54:39 +0300

    remove resolving hosts

--- core/postfix/start.py
+++ core/postfix/start.py
@@ -32,10 +32,10 @@ def is_valid_postconf_line(line):
             and not line == ''
 
 # Actual startup script
-os.environ["FRONT_ADDRESS"] = system.get_host_address_from_environment("FRONT", "front")
-os.environ["ADMIN_ADDRESS"] = system.get_host_address_from_environment("ADMIN", "admin")
-os.environ["ANTISPAM_MILTER_ADDRESS"] = system.get_host_address_from_environment("ANTISPAM_MILTER", "antispam:11332")
-os.environ["LMTP_ADDRESS"] = system.get_host_address_from_environment("LMTP", "imap:2525")
+os.environ["FRONT_ADDRESS"] = os.getenv("FRONT_ADDRESS",  default="front")
+os.environ["ADMIN_ADDRESS"] = os.getenv("ADMIN_ADDRESS", default="admin")
+os.environ["ANTISPAM_MILTER_ADDRESS"] = os.getenv("ANTISPAM_MILTER", default="antispam:11332")
+os.environ["LMTP_ADDRESS"] = os.getenv("LMTP", default="imap:2525")
 
 for postfix_file in glob.glob("/conf/*.cf"):
     conf.jinja(postfix_file, os.environ, os.path.join("/etc/postfix", os.path.basename(postfix_file)))
--- core/rspamd/start.py
+++ core/rspamd/start.py
@@ -10,11 +10,11 @@ log.basicConfig(stream=sys.stderr, level=os.environ.get("LOG_LEVEL", "WARNING"))
 
 # Actual startup script
 
-os.environ["FRONT_ADDRESS"] = system.get_host_address_from_environment("FRONT", "front")
-os.environ["REDIS_ADDRESS"] = system.get_host_address_from_environment("REDIS", "redis")
+os.environ["FRONT_ADDRESS"] = os.getenv("FRONT_ADDRESS", default="front")
+os.environ["REDIS_ADDRESS"] = os.getenv("REDIS_ADDRESS", default="redis")
 
 if os.environ.get("ANTIVIRUS") == 'clamav':
-    os.environ["ANTIVIRUS_ADDRESS"] = system.get_host_address_from_environment("ANTIVIRUS", "antivirus:3310")
+    os.environ["ANTIVIRUS_ADDRESS"] = os.getenv("ANTIVIRUS_ADDRESS", default="antivirus:3310")
 
 for rspamd_file in glob.glob("/conf/*"):
     conf.jinja(rspamd_file, os.environ, os.path.join("/etc/rspamd/local.d", os.path.basename(rspamd_file)))
